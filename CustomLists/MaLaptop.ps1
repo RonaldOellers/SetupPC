641d55c1d0b70aee561aa94fd5148426185dc6c0
Ronald Oellers <83066268+RonaldOellers@users.noreply.github.com>
2023-06-30 23:54:31 +0200

    Create MaLaptop.ps1

---
+++ CustomLists/MaLaptop.ps1
@@ -0,0 +1,36 @@
+$apps = @(
+    #Communication
+    @{name = "WhatsApp.WhatsApp"; location = "C:\Programme\WhatsApp\" },
+    @{name = "Zoom.Zoom"; location = "C:\Programme\Zoom\" },
+
+    #Coding and utilty  
+    @{name = "Microsoft.PowerToys" },
+    @{name = "Notepad++.Notepad++"; location = "C:\Programme\Notepad++\" },
+    @{name = "EclipseAdoptium.Temurin.19.JDK" },#Java especialy for Minecraft
+
+    #Browser
+    @{name = "Mozilla.Firefox"; location = "C:\Programme\Firefox\" },
+    @{name = "Google.Chrome"; location = "C:\Programme\GoogleChrome\" },
+
+    #Writing and Office
+    @{name = "Adobe.Acrobat.Reader.64-bit"; location = "C:\Programme\Adobe\AcrobatReader" },#Acrobat Reader
+    @{name = "TrackerSoftware.PDF-XChangeEditor"; location = "C:\Programme\PDF_XChange\" },#PDF XChange Editor
+
+    #Monitoring
+    @{name = "CPUID.HWMonitor"; location = "C:\Programme\HWmonitor\" },#HW Monitor
+    @{name = "CrystalDewWorld.CrystalDiskInfo"; location = "C:\Programme\CrystalDiskInfo\" },#Crystal Disc info
+
+    #Utility Software
+    @{name = "RARLab.WinRAR"; location = "C:\Programme\Winrar\" },#Winrar
+    @{name = "VideoLAN.VLC"; location = "C:\Programme\VLC\" },#VLC Media Player
+    @{name = "Piriform.Recuva"; location = "C:\Programme\Recuva\" },#data recovery
+
+    #Benchmarking
+    @{name = "9PGZKJC81Q7J"; location = "C:\Programme\Benchmark\Cinebench" },#Cinebench
+    @{name = "CPUID.CPU-Z"; location = "C:\Programme\Benchmark\CPUZ\"  },#CPU-Z
+    @{name = "CrystalDewWorld.CrystalDiskMark"; location = "C:\Programme\Benchmark\CrystalDiskMark\"  },#Crystal Disc mark
+
+    );
+
+#Finisher 
+Write-host "Loaded Custom Apps List"#Leave this in the file as it is needed to run this file seperately!!!
